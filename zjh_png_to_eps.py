806d2c0e66acfe502861bb1103d203bc84be0893
zoujinhang <653538607@qq.com>
2020-03-14 15:30:26 +0800

    修改

--- zjh_png_to_eps.py
+++ zjh_png_to_eps.py
@@ -12,8 +12,6 @@ import os
 def png_to_eps(fit,out):
 	cmd = 'convert -density 3000 '+fit+' '+out
 	os.system(cmd)
-
-
 #fit = '/home/laojin/result/bn200219317_zbb/B_txx_15-350.png'
 #out = '/home/laojin/result/bn200219317_zbb/B_txx_15-350.eps'
 #png_to_eps(fit,out)
